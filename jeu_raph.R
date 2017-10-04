9f84e0a2280ed6a8ab82986e23fcb2ede99d61d9
Hamza Sentissi <hamza.sentissi@hec.edu>
2017-10-04 15:37:43 +0200

    1st version :)

---
+++ jeu_raph.R
@@ -0,0 +1,28 @@
+raph_pd2 <- function(x) {
+  a = runif(1, 0, 10)
+  if (a > 3){
+    print("Raph is payday")
+  } else {
+    print("Il est chanceux ce petit cocu")
+  }
+  x <- x+1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+raph_pd2(1)
+
+
+
